213a83bbe92f861c8850cc73e35cb3eb06cd0c57
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-06-21 15:49:54 -0500

    :sparkles: feat(keymap): convert `KC_RALT` into `RALT_T(KC_APP)`

--- keymap.c
+++ keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS, \
   SFT_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
   KC_LCTL,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY, KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,  \
-                           KC_LALT, KC_LGUI, MO(_LOWER),       KC_SPC,  KC_SPC,  MO(_RAISE),       KC_RALT, KC_RCTL                     \
+                           KC_LALT, KC_LGUI, MO(_LOWER),       KC_SPC,  KC_SPC,  MO(_RAISE),       RALT_T(KC_APP),   KC_RCTL                     \
 ),
 
 [_LOWER] = LAYOUT( \
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX, \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-                    _______, _______, MO(_EXTRA),       _______, _______, _______,            _______, _______                   \
+                    _______, _______, MO(_EXTRA),       _______, _______, _______,          _______, _______                    \
 ),
 
 [_EXTRA] = LAYOUT( \
