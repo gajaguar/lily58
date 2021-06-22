5cd4352fad5925d68ce6e46c6fe55218d80dada0
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-06-21 19:13:50 -0500

    :sparkles: feat(layers): add `game`

--- keymap.c
+++ keymap.c
@@ -8,10 +8,17 @@ enum layer_number {
   _GAME,
 };
 
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define EXTRA MO(_EXTRA)
+#define QWERTY TO(_QWERTY)
+#define GAME TO(_GAME)
+#define SP_CAPS SFT_T(KC_CAPS)
+#define SP_RALT RALT_T(KC_APP)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY */
-/* ADJUST
+/* BASE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -21,25 +28,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Space \  |RAISE |      |      |
+ *                   |      |      |LOWER | /Space  /       \Space \  |RAISE |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC, \
-  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS, \
-  SFT_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
-  KC_LCTL,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY, KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,  \
-                           KC_LALT, KC_LGUI, MO(_LOWER),       KC_SPC,  KC_SPC,  MO(_RAISE),       RALT_T(KC_APP),   KC_RCTL                     \
+  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS, \
+  SP_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
+  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY, KC_DEL,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,  \
+                             KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,  SP_RALT, KC_RCTL                              \
 ),
 
 [_LOWER] = LAYOUT( \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,                   KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX, _______, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,                   KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX, XXXXXXX, \
-  _______, KC_SLSH, S(KC_8), KC_MINS, S(KC_EQL), KC_EQL,                    KC_1,    KC_2,    KC_3,    XXXXXXX, XXXXXXX, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______, _______, KC_0,    KC_0,    KC_DOT,  XXXXXXX, XXXXXXX, _______, \
-                    _______, _______, _______,            _______, _______, MO(_EXTRA),       _______, _______                    \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK,                   KC_P7,   KC_P8,   KC_P9,   XXXXXXX, XXXXXXX, _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX, XXXXXXX, \
+  _______, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_EQL,                    KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX, XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_P0,   KC_P0,   KC_PDOT, XXXXXXX, XXXXXXX, _______, \
+                             _______, _______, _______, _______, _______, EXTRA,   _______, _______                             \
 ),
 
 [_RAISE] = LAYOUT( \
@@ -47,31 +54,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX, \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-                    _______, _______, MO(_EXTRA),       _______, _______, _______,          _______, _______                    \
+                             _______, _______, EXTRA,   _______, _______, _______, _______, _______                             \
 ),
 
 [_EXTRA] = LAYOUT( \
-XXXXXXX, TO(_LOWER), TO(_RAISE), TO(_EXTRA), TO(_GAME), XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-XXXXXXX, KC_GRV,     KC_LT,      KC_LPRN,    KC_LCBR,   KC_LBRC,                   KC_RBRC, KC_RCBR, KC_RPRN, KC_GT,   KC_UNDS, XXXXXXX, \
-XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-                     _______, _______, _______,         _______, _______,  _______,         _______, _______                             \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GAME,    \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, KC_GRV,  KC_LT,   KC_LPRN, KC_LCBR, KC_LBRC,                   KC_RBRC, KC_RCBR, KC_RPRN, KC_GT,   KC_UNDS, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+                             _______, _______, _______, _______, _______, _______, _______, _______                             \
 ),
 
 [_GAME] = LAYOUT( \
-KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC, \
-XXXXXXX, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS, \
-XXXXXXX, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
-XXXXXXX, KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,  \
-                           _______, _______, _______, _______, _______,  _______, _______, _______                             \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    QWERTY,  \
+  KC_T,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+  KC_G,    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_B,    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_MPLY, KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+                             KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   SP_RALT, KC_RCTL                             \
 )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _EXTRA);
+// Setting ADJUST layer RGB back to default
+void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+    if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+        layer_on(layer3);
+    } else {
+        layer_off(layer3);
+    }
 }
 
-//SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
 #ifdef OLED_DRIVER_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -94,15 +105,34 @@ const char *read_keylogs(void);
 
 void oled_task_user(void) {
   if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+    case _QWERTY:
+        oled_write_ln_P(PSTR("Default"), false);
+        break;
+    case _LOWER:
+        oled_write_ln_P(PSTR("Num Lock"), false);
+        break;
+    case _RAISE:
+        oled_write_ln_P(PSTR("Functions"), false);
+        break;
+    case _EXTRA:
+        oled_write_ln_P(PSTR("Special"), false);
+        break;
+    case _GAME:
+        oled_write_ln_P(PSTR("Game"), false);
+        break;
+    default:
+        oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
     oled_write_ln(read_keylog(), false);
     oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
+
   } else {
-    oled_write(read_logo(), false);
+      oled_write(read_logo(), false);
   }
 }
 #endif // OLED_DRIVER_ENABLE
