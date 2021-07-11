0e80aeccfff297adee95e889d1208638022e09ac
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-07-11 18:20:48 -0500

    :sparkles: feat(keymap): implement tap dance

--- keymap.c
+++ keymap.c
@@ -8,13 +8,29 @@ enum layer_number {
   _GAME,
 };
 
+// Tap Dance declarations
+enum {
+    TD_LSFT_CAPS,
+    TD_N_ENIE,
+    TD_SINGLE_DOUBLE,
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    [TD_N_ENIE] = ACTION_TAP_DANCE_DOUBLE(KC_N, RALT(KC_N)),
+    [TD_SINGLE_DOUBLE] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, S(KC_QUOT)),
+};
+
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define EXTRA MO(_EXTRA)
 #define QWERTY TO(_QWERTY)
 #define GAME TO(_GAME)
-#define SP_CAPS SFT_T(KC_CAPS)
 #define SP_RALT RALT_T(KC_APP)
+#define TD_CAPS TD(TD_LSFT_CAPS)
+#define TD_ENIE TD(TD_N_ENIE)
+#define TD_QUOT TD(TD_SINGLE_DOUBLE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -34,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS, \
-  SP_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
-  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY, KC_DEL,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,  \
-                             KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,  SP_RALT, KC_RCTL                              \
+  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS, \
+  TD_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  TD_QUOT, \
+  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY, KC_DEL,  TD_ENIE, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,  \
+                             KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   SP_RALT, KC_RCTL                              \
 ),
 
 [_LOWER] = LAYOUT( \
