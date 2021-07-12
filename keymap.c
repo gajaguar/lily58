b844435c92059457fbfd150fd62500d2f515e9a0
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-07-11 19:21:51 -0500

    :sparkles: feat(keymap): convert `KC_SLSH` into `TD_QSTN`

--- keymap.c
+++ keymap.c
@@ -15,6 +15,7 @@ enum {
     TD_SINGLE_DOUBLE,
     TD_LEFT_BRACKET_BRACE,
     TD_RIGHT_BRACKET_BRACE,
+    TD_QUESTION,
 };
 
 // Tap Dance definitions
@@ -24,6 +25,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_SINGLE_DOUBLE] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, S(KC_QUOT)),
     [TD_LEFT_BRACKET_BRACE] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
     [TD_RIGHT_BRACKET_BRACE] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR),
+    [TD_QUESTION] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, RALT(KC_SLSH)),
 };
 
 #define LOWER MO(_LOWER)
@@ -37,6 +39,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define TD_QUOT TD(TD_SINGLE_DOUBLE)
 #define TD_LBRC TD(TD_LEFT_BRACKET_BRACE)
 #define TD_RBRC TD(TD_RIGHT_BRACKET_BRACE)
+#define TD_QSTN TD(TD_QUESTION)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -59,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS, \
   TD_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  TD_QUOT, \
-  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY, KC_DEL,  TD_ENIE, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,  \
+  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY, KC_DEL,  TD_ENIE, KC_M,    KC_COMM, KC_DOT,  TD_QSTN,  KC_ENT,  \
                              KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   SP_RALT, KC_RCTL                              \
 ),
 
